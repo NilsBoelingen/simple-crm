49033c33ba0b839249f0dd21d7bf6579a5e8c116
Nils Boelingen <54119982+NilsBoelingen@users.noreply.github.com>
2024-01-21 23:17:37 +0100

    add some functions

---
+++ src/app/add-user-dialog/add-user-dialog.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { User } from '../../models/user.class';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-add-user-dialog',
+  standalone: true,
+  providers: [provideNativeDateAdapter()],
+  imports: [MatButtonModule, MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatFormFieldModule, MatInputModule, MatDatepickerModule, FormsModule],
+  templateUrl: './add-user-dialog.component.html',
+  styleUrl: './add-user-dialog.component.scss'
+})
+export class AddUserDialogComponent {
+  user = new User();
+}
