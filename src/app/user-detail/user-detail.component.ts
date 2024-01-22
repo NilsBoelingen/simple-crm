4872ef8a1cca9fa157c7e33f06eff5ccba05b5d9
Nils Boelingen <54119982+NilsBoelingen@users.noreply.github.com>
2024-01-22 22:59:47 +0100

    work on user detail

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Firestore, Unsubscribe, doc, onSnapshot } from '@angular/fire/firestore';
+import { MatCardModule } from '@angular/material/card';
+import { ActivatedRoute } from '@angular/router';
+import { User } from '../../models/user.class';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss'
 })
 export class UserDetailComponent {
+  firestore: Firestore = inject(Firestore);
+  userId: string = '';
+  unSubUser: any;
+  user: User = new User();
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe( paramMap => {
+      this.userId = paramMap.get('id')!.toString();
+      this.getUser();
+  })
+  }
+
+  ngOnDestroy() {
+    this.unSubUser();
+  }
+
+  getUser() {
+    this.unSubUser = onSnapshot(doc(this.firestore, 'users', this.userId), (user) => {
+      this.user = new User(user.data());
+    });
+  }
 }
