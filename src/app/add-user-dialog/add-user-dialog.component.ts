12523f8b541dcf146145bc5219c9ccab9f1e6e60
Nils Boelingen <54119982+NilsBoelingen@users.noreply.github.com>
2024-01-22 13:16:26 +0100

    work on dialog

--- src/app/add-user-dialog/add-user-dialog.component.ts
+++ src/app/add-user-dialog/add-user-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -7,21 +7,34 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { User } from '../../models/user.class';
 import { FormsModule } from '@angular/forms';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-add-user-dialog',
   standalone: true,
   providers: [provideNativeDateAdapter()],
-  imports: [MatButtonModule, MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatFormFieldModule, MatInputModule, MatDatepickerModule, FormsModule],
+  imports: [MatButtonModule, MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatFormFieldModule, MatInputModule, MatDatepickerModule, FormsModule, MatProgressBarModule, NgIf],
   templateUrl: './add-user-dialog.component.html',
   styleUrl: './add-user-dialog.component.scss'
 })
 export class AddUserDialogComponent {
+  firestore: Firestore = inject(Firestore);
   user = new User();
   birthDate!: Date;
+  loading: boolean = false;
 
-  saveUser() {
-    this.user.birthDate = this.birthDate ? this.birthDate.getTime(): 0;
+  constructor() {
+
+  }
+
+  async saveUser() {
+    this.user.birthDate = this.birthDate ? this.birthDate.getTime() : 0;
     console.log(this.user);
+    this.loading = true;
+    const docRef = await addDoc(collection(this.firestore, "users"), this.user.toJSON());
+    this.loading = false;
+    console.log("Document written with ID: ", docRef.id);
   }
 }
