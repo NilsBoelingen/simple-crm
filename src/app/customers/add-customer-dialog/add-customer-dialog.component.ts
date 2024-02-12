65c181e83bab44fd2519a34b5d3f95ff39029972
Nils Boelingen <54119982+NilsBoelingen@users.noreply.github.com>
2024-02-12 13:22:22 +0100

    add form control

--- src/app/customers/add-customer-dialog/add-customer-dialog.component.ts
+++ src/app/customers/add-customer-dialog/add-customer-dialog.component.ts
@@ -1,11 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { provideNativeDateAdapter } from '@angular/material/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { NgIf } from '@angular/common';
 import { FirestoreService } from '../../services/firestore/firestore.service';
@@ -14,11 +14,27 @@ import { FirestoreService } from '../../services/firestore/firestore.service';
   selector: 'app-add-customer-dialog',
   standalone: true,
   providers: [provideNativeDateAdapter()],
-  imports: [MatButtonModule, MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatFormFieldModule, MatInputModule, MatDatepickerModule, FormsModule, MatProgressBarModule, NgIf],
+  imports: [MatButtonModule, MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatFormFieldModule, MatInputModule, MatDatepickerModule, FormsModule, MatProgressBarModule, NgIf, ReactiveFormsModule],
   templateUrl: './add-customer-dialog.component.html',
   styleUrl: './add-customer-dialog.component.scss'
 })
 export class AddCustomerDialogComponent {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  firstName = new FormControl('', [Validators.required])
+  lastName = new FormControl('', [Validators.required])
+  city = new FormControl('', [Validators.required])
+  zipCode = new FormControl('', [Validators.required, Validators.min(5), Validators.max(5)])
+  address = new FormControl('', [Validators.required])
+  telephone = new FormControl('', [Validators.required, Validators.min(8)])
 
   constructor(public firestore: FirestoreService) {}
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
 }
--- src/app/products/add-product-dialog/add-product-dialog.component.ts
+++ src/app/products/add-product-dialog/add-product-dialog.component.ts
@@ -3,7 +3,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { NgIf } from '@angular/common';
 import { Product } from '../../../models/product.class';
@@ -12,12 +12,14 @@ import { FirestoreService } from '../../services/firestore/firestore.service';
 @Component({
   selector: 'app-add-product-dialog',
   standalone: true,
-  imports: [MatButtonModule, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle, MatFormFieldModule, MatInputModule, FormsModule, MatProgressBarModule, NgIf],
+  imports: [MatButtonModule, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle, MatFormFieldModule, MatInputModule, FormsModule, MatProgressBarModule, NgIf, ReactiveFormsModule],
   templateUrl: './add-product-dialog.component.html',
   styleUrl: './add-product-dialog.component.scss'
 })
 export class AddProductDialogComponent {
   newProduct: Product = new Product();
+  name = new FormControl('', [Validators.required]);
+  price = new FormControl('', [Validators.required])
 
   constructor(public firestore: FirestoreService) {}
 
